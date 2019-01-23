74760326e01b7a62ebfec08de4f9deccf8f6acc6
716753 <sergio.orcajoperez@cognizant.com>
2019-01-23 12:39:30 +0100

    Implementing the main UI

--- app/src/main/java/com/example/kphisics/ui/projects/ProjectsNavFragment.kt
+++ app/src/main/java/com/example/kphisics/ui/projects/ProjectsNavFragment.kt
@@ -1,7 +1,14 @@
 package com.example.kphisics.ui.projects
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.kphisics.R
 
 class ProjectsNavFragment : Fragment() {
-
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_projects, container, false)
+    }
 }
--- app/src/main/java/com/example/kphisics/ui/tools/ToolsNavFragment.kt
+++ app/src/main/java/com/example/kphisics/ui/tools/ToolsNavFragment.kt
@@ -1,7 +1,19 @@
 package com.example.kphisics.ui.tools
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
 class ToolsNavFragment : Fragment(){
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 
 }
